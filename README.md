3c3463fb0f603776c11781f8805248f3fc4d1bb0
Dean Wilson <dean.wilson@gmail.com>
2014-04-10 16:24:15 +0100

    Add sample ulimit structured fact

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ A collection of my facter facts that don't fit better in more specific repos
 
 yumplugins :: Returns a data structure detailing the yum plugins present, enabled and disabled on the system. Requires facter2 and uses the structured fact support.
 
+ulimit :: Returns a data structure containing ulimit values for the currently running process.
+
 scl_enabled :: Returns a csv list of software collections available on this host
 
 scl_available :: the software collection puppet/facter is running under
---
+++ lib/facter/ulimit.rb
@@ -0,0 +1,47 @@
+# Fact: ulimit
+#
+# Purpose:
+#   Returns a hash containing ulimit values for the currently
+#   running process.
+#
+#   Each limit entry contains keys representing the unit, soft limit and
+#   hard limit.
+#
+# Resolution:
+#   Reads the contents of /proc/$pid/limits
+#
+# Caveats:
+#   Uses $::kernel to filter to only run on Linux. May work on other *nix
+#   Requires facter 2 for structured facts.
+
+Facter.add('ulimit') do
+  confine :kernel => 'Linux'
+
+  setcode do
+    pid = Process.pid
+    proc_file = "/proc/#{pid}/limits"
+
+    limits = File.readlines(proc_file)
+    limits.shift
+ 
+    # priority ulimits don't have a unit so normalise them.
+    limits.grep(/priority/).map { |limit| limit.gsub!(/\n/, 'niceness') }
+
+    process_limits = {}
+    limits.each do |limit|
+      limit = limit.split
+
+      desc_length = (limit.length - 4)
+
+      desc = limit[0..desc_length].join('_').downcase
+
+      process_limits[desc] = {
+                               'soft' => limit[-3],
+                               'hard' => limit[-2],
+                               'unit' => limit[-1]
+                             }
+    end
+
+    process_limits
+  end
+end
