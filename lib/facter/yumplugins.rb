5f8b4aa719c97ca6f20990f1d1f9ff727a7a64f2
Dean Wilson <dean.wilson@gmail.com>
2016-01-22 23:27:03 +0000

    Rubocop tidy

--- lib/facter/yumplugins.rb
+++ lib/facter/yumplugins.rb
@@ -1,6 +1,6 @@
 # Fact: yumplugins
 #
-# Purpose: 
+# Purpose:
 #   Returns a hash of arrays detailing the yum plugins present,
 #   enabled and disabled
 #
@@ -11,31 +11,29 @@
 # Caveats:
 #   Uses osfamily to filter. Only runs on RedHat family hosts
 #   Requires facter 2 for structured facts.
-
-Facter.add("yumplugins") do
-  confine :osfamily => "RedHat"
+Facter.add('yumplugins') do
+  confine osfamily: 'RedHat'
 
   setcode do
     plugin_directory = '/etc/yum/pluginconf.d'
- 
-    plugins = {}
-    plugins['plugin']   = [] 
-    plugins['enabled']  = [] 
-    plugins['disabled'] = [] 
 
-    Dir[plugin_directory + '/*.conf'].each do | file |
-      plugin_name = File.basename( file, '.conf')
-  
+    plugins = {}
+    plugins['plugin']   = []
+    plugins['enabled']  = []
+    plugins['disabled'] = []
+
+    Dir[plugin_directory + '/*.conf'].each do |file|
+      plugin_name = File.basename(file, '.conf')
+
       plugins['plugin'] << plugin_name
-  
+
       enabled = File.open(file).read.grep(/^(\s*enabled\s*=\s*[01])/).to_s.split('=')[1]
-  
+
       if enabled.to_i.zero?
         plugins['disabled'] << plugin_name
       else
         plugins['enabled'] << plugin_name
       end
-  
     end
 
     plugins.keys.each do |key|
